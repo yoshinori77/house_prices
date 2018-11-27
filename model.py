b06b6fcd63780510891987f464312850bedffddf
yoshinori77 <yoshinoriyamashita123@gmail.com>
2018-11-27 11:00:37 +0900

    boxcox変換など追加

--- model.py
+++ model.py
@@ -10,6 +10,11 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_squared_log_error
 from xgboost import XGBRegressor
 import lightgbm as lgb
+from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
+from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import make_pipeline
+from scipy.stats import norm, skew
 
 pd.set_option("display.max_rows", 200)
 pd.set_option('display.max_columns', 200)
@@ -111,12 +116,17 @@ left_time = left_time.apply(lambda x: 1 if x < 1 else x)
 all_data['QualLefttime'] = all_data['OverallQual'] * left_time
 all_data['TotalSFLefttime'] = all_data['TotalSF'] * left_time
 all_data['TotalSFQualLefttime'] = all_data['OverallQual'] * all_data['TotalSF'] * left_time
+all_data["TotalSFOverallQual"] = all_data["TotalSF"] * all_data["OverallQual"]
 
 all_data['TotalRoomNum'] = all_data['TotRmsAbvGrd'] + all_data['BsmtFullBath'] + \
                             all_data['BsmtHalfBath'] + all_data['FullBath'] + \
                             all_data['HalfBath']
 all_data['TotalRoomNumLefttime'] = all_data['TotalRoomNum'] * left_time
 
+[c for c in all_data.columns if 'Fire' in c]
+all_data['FireplaceQu_Ex']
+all_data[['Fireplaces','FireplaceQu_Ex','FireplaceQu_Fa','FireplaceQu_Gd','FireplaceQu_None','FireplaceQu_Po','FireplaceQu_TA']].hist()
+all_data[['Foundation_BrkTil', 'Foundation_CBlock', 'Foundation_PConc', 'Foundation_Slab', 'Foundation_Stone', 'Foundation_Wood']].hist()
 remodel_left_time = 55 - (2018 - all_data['YearRemodAdd'])
 remodel_left_time = remodel_left_time.apply(lambda x: 1 if x < 1 else x)
 all_data['QualRemodLefttime'] = all_data['OverallQual'] * remodel_left_time
@@ -124,8 +134,18 @@ all_data['TotalSFRemodLefttime'] = all_data['TotalSF'] * remodel_left_time
 all_data['TotalSFQualRemodLefttime'] = all_data['OverallQual'] * all_data['TotalSF'] * remodel_left_time
 
 all_data['TotalRoomNumRemodLefttime'] = all_data['TotalRoomNum'] * remodel_left_time
+all_data['BuiltLehmanFlg'] = all_data['YearBuilt'].apply(lambda x: 1 if 2005 < x < 2011 else 0)
+all_data['SoldLehmanFlg'] = all_data['YrSold'].apply(lambda x: 1 if x > 2007 else 0)
 
-all_data['LehmanFlg'] = all_data['YearBuilt'].apply(lambda x: 1 if 2005 < x < 2011 else 0)
+numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+numeric_feats
+# Check the skew of all numerical features
+skewed_feats = all_data[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+print("\nSkew in numerical features: \n")
+skewness = pd.DataFrame({'Skew' :skewed_feats})
+all_data['Exterior2nd_Other']
+skewness.head(15)
+
 # X_train = preprocess(X_train, high_corr_columns, missing_columns2)
 # X_val = preprocess(X_val, high_corr_columns, missing_columns2)
 # X_test = preprocess(X_test, high_corr_columns, missing_columns2)
@@ -140,7 +160,6 @@ y_val = y_val.reset_index(drop=True)
 # lgb_train = lgb.Dataset(X_train, y_train, categorical_feature=list(category_columns))
 # lgb_val = lgb.Dataset(X_val, y_val, categorical_feature=list(category_columns))
 
-
 # category_columns
 # for column in category_columns:
 #     X_train[column] = pd.Categorical(X_train[column])
@@ -148,23 +167,55 @@ y_val = y_val.reset_index(drop=True)
 #     X_test[column] = pd.Categorical(X_test[column])
 
 
-model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=10,
-                              learning_rate=0.05, n_estimators=720,
+model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
+                              learning_rate=0.05, n_estimators=1000,
                               max_bin = 55, bagging_fraction = 0.8,
                               bagging_freq = 5, feature_fraction = 0.2319,
                               feature_fraction_seed=9, bagging_seed=9,
                               min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
-score = rmsle_cv(model_lgb, 5, all_data[:ntrain], y)
-print("LGBM score: {:.4f} ({:.4f})\n" .format(score.mean(), score.std()))
+clf = XGBRegressor()
+score_lightgbm = rmsle_cv(model_lgb, 5, all_data[:ntrain], y)
+score_xgboost = rmsle_cv(clf, 5, all_data[:ntrain], y)
+print("LGBM score: {:.4f} ({:.4f})\n" .format(score_lightgbm.mean(),
+                                              score_lightgbm.std()))
+print("XGBOOST score: {:.4f} ({:.4f})\n" .format(score_xgboost.mean(),
+                                              score_xgboost.std()))
+score_lightgbm
+score_xgboost
 
-model = model_lgb.fit(all_data[:ntrain], y)
+GBoost = GradientBoostingRegressor(n_estimators=1000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10,
+                                   loss='huber', random_state =5)
+score_gbdt = rmsle_cv(GBoost, 5, all_data[:ntrain], y)
+print("Gradient Boosting score: {:.4f} ({:.4f})\n".format(score_gbdt.mean(), score_gbdt.std()))
+score_gbdt
+np.mean(score_lightgbm.tolist()+score_xgboost.tolist()+score_gbdt.tolist())
 
-all_data.drop([col for col, f in zip(all_data.columns, model.feature_importances_) if f < 5], axis=1, inplace=True)
+ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.1, l1_ratio=.9, random_state=3))
+score_elasticnet = rmsle_cv(ENet, 5, all_data[:ntrain], y)
+print("ElasticNet score: {:.4f} ({:.4f})\n".format(score_elasticnet.mean(), score_elasticnet.std()))
+score_elasticnet
+score_gbdt
+score_lightgbm
+score_xgboost
+
+
+
+numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+
+# Check the skew of all numerical features
+skewed_feats = all_data[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+print("\nSkew in numerical features: \n")
+skewness = pd.DataFrame({'Skew' :skewed_feats})
+skewness.head(15)model = model_lgb.fit(all_data[:ntrain], y)
+
+# all_data.drop([col for col, f in zip(all_data.columns, model.feature_importances_) if f < 5], axis=1, inplace=True)
+#
+# score = rmsle_cv(model_lgb, 5, all_data[:ntrain], y)
+# print("LGBM score: {:.4f} ({:.4f})\n" .format(score.mean(), score.std()))
+
 X_test = all_data[ntrain:]
-
-score = rmsle_cv(model_lgb, 5, all_data[:ntrain], y)
-print("LGBM score: {:.4f} ({:.4f})\n" .format(score.mean(), score.std()))
-
 model = model_lgb.fit(all_data[:ntrain], y)
 y_pred = model.predict(X_test)
 
