5a69d98b4c00c9ae3f86b66427065b0b3fe78344
yoshinori77 <yoshinoriyamashita123@gmail.com>
2018-11-27 11:04:39 +0900

    DLの回帰を実施（特徴量を追加してもあまり効果なかった & カテゴリ変数をあまり活用できてない)

---
+++ model_diego2.py
@@ -0,0 +1,220 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import os
+import keras
+import itertools
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import rcParams
+import matplotlib
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+train = pd.read_csv('./input/train.csv')
+test = pd.read_csv('./input/test.csv')
+test_ID = test['Id']
+
+ntrain = train.shape[0]
+ntest = test.shape[0]
+y_train = train.SalePrice.values
+
+all_data = pd.concat((train, test)).reset_index(drop=True)
+# all_data.drop(['SalePrice'], axis=1, inplace=True)
+print("all_data size is : {}".format(all_data.shape))
+
+all_data["PoolQC"] = all_data["PoolQC"].fillna("None")
+
+all_data["MiscFeature"] = all_data["MiscFeature"].fillna("None")
+
+all_data["Alley"] = all_data["Alley"].fillna("None")
+
+all_data["Fence"] = all_data["Fence"].fillna("None")
+
+all_data["FireplaceQu"] = all_data["FireplaceQu"].fillna("None")
+
+all_data["LotFrontage"] = all_data.groupby("Neighborhood")["LotFrontage"].transform(lambda x: x.fillna(x.median()))
+
+for col in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond'):
+    all_data[col] = all_data[col].fillna('None')
+
+for col in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
+    all_data[col] = all_data[col].fillna(0)
+
+for col in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF','TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath'):
+    all_data[col] = all_data[col].fillna(0)
+
+for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+    all_data[col] = all_data[col].fillna('None')
+
+all_data["MasVnrType"] = all_data["MasVnrType"].fillna("None")
+all_data["MasVnrArea"] = all_data["MasVnrArea"].fillna(0)
+
+all_data['MSZoning'] = all_data['MSZoning'].fillna(all_data['MSZoning'].mode()[0])
+
+all_data = all_data.drop(['Utilities'], axis=1)
+
+all_data["Functional"] = all_data["Functional"].fillna("Typ")
+
+all_data['Electrical'] = all_data['Electrical'].fillna(all_data['Electrical'].mode()[0])
+
+all_data['KitchenQual'] = all_data['KitchenQual'].fillna(all_data['KitchenQual'].mode()[0])
+
+all_data['Exterior1st'] = all_data['Exterior1st'].fillna(all_data['Exterior1st'].mode()[0])
+all_data['Exterior2nd'] = all_data['Exterior2nd'].fillna(all_data['Exterior2nd'].mode()[0])
+
+all_data['SaleType'] = all_data['SaleType'].fillna(all_data['SaleType'].mode()[0])
+
+all_data['MSSubClass'] = all_data['MSSubClass'].fillna("None")
+
+#Check remaining missing values if any
+all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)
+missing_data = pd.DataFrame({'Missing Ratio' :all_data_na})
+missing_data.head()
+
+#MSSubClass=The building class
+all_data['MSSubClass'] = all_data['MSSubClass'].apply(str)
+
+#Changing OverallCond into a categorical variable
+all_data['OverallCond'] = all_data['OverallCond'].astype(str)
+
+#Year and month sold are transformed into categorical features.
+all_data['YrSold'] = all_data['YrSold'].astype(str)
+all_data['MoSold'] = all_data['MoSold'].astype(str)
+
+from sklearn.preprocessing import LabelEncoder
+cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
+        'ExterQual', 'ExterCond','HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1',
+        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond',
+        'YrSold', 'MoSold')
+# process columns, apply LabelEncoder to categorical features
+for c in cols:
+    lbl = LabelEncoder()
+    lbl.fit(list(all_data[c].values))
+    all_data[c] = lbl.transform(list(all_data[c].values))
+
+# shape
+print('Shape all_data: {}'.format(all_data.shape))
+
+# Adding total sqfootage feature
+all_data['TotalSF'] = all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']
+
+train = all_data[:ntrain]
+test = all_data[ntrain:]
+test = test.drop('SalePrice', axis=1)
+
+print('Shape of the train data with all features:', train.shape)
+train = train.select_dtypes(exclude=['object'])
+print("")
+print('Shape of the train data with numerical features:', train.shape)
+train.drop('Id',axis = 1, inplace = True)
+train.fillna(0,inplace=True)
+
+test = test.select_dtypes(exclude=['object'])
+ID = test.Id
+test.fillna(0,inplace=True)
+test.drop('Id',axis = 1, inplace = True)
+
+print("")
+print("List of features contained our dataset:",list(train.columns))
+
+from sklearn.ensemble import IsolationForest
+
+clf = IsolationForest(max_samples = 100, random_state = 42)
+clf.fit(train)
+y_noano = clf.predict(train)
+y_noano = pd.DataFrame(y_noano, columns = ['Top'])
+y_noano[y_noano['Top'] == 1].index.values
+
+train = train.iloc[y_noano[y_noano['Top'] == 1].index.values]
+train.reset_index(drop = True, inplace = True)
+print("Number of Outliers:", y_noano[y_noano['Top'] == -1].shape[0])
+print("Number of rows without outliers:", train.shape[0])
+
+import warnings
+warnings.filterwarnings('ignore')
+
+col_train = list(train.columns)
+col_train_bis = list(train.columns)
+
+col_train_bis.remove('SalePrice')
+
+mat_train = np.matrix(train)
+mat_test  = np.matrix(test)
+mat_new = np.matrix(train.drop('SalePrice',axis = 1))
+mat_y = np.array(train.SalePrice).reshape((1314,1))
+
+prepro_y = MinMaxScaler()
+prepro_y.fit(mat_y)
+
+prepro = MinMaxScaler()
+prepro.fit(mat_train)
+
+prepro_test = MinMaxScaler()
+prepro_test.fit(mat_new)
+
+train = pd.DataFrame(prepro.transform(mat_train),columns = col_train)
+test  = pd.DataFrame(prepro_test.transform(mat_test),columns = col_train_bis)
+
+# List of features
+COLUMNS = col_train
+FEATURES = col_train_bis
+LABEL = "SalePrice"
+
+# Columns
+feature_cols = FEATURES
+
+# Training set and Prediction set with the features to predict
+training_set = train[COLUMNS]
+prediction_set = train.SalePrice
+
+# Train and Test
+x_train, x_test, y_train, y_test = train_test_split(training_set[FEATURES] , prediction_set, test_size=0.33, random_state=42)
+y_train = pd.DataFrame(y_train, columns = [LABEL])
+training_set = pd.DataFrame(x_train, columns = FEATURES).merge(y_train, left_index = True, right_index = True)
+training_set.head()
+
+# Training for submission
+training_sub = training_set[col_train]
+
+# Same thing but for the test set
+y_test = pd.DataFrame(y_test, columns = [LABEL])
+testing_set = pd.DataFrame(x_test, columns = FEATURES).merge(y_test, left_index = True, right_index = True)
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+
+seed = 7
+np.random.seed(seed)
+
+# Model
+model = Sequential()
+model.add(Dense(200, input_dim=59, kernel_initializer='normal', activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(25, activation='relu'))
+model.add(Dense(1))
+# Compile model
+model.compile(loss='mean_squared_logarithmic_error', optimizer=keras.optimizers.Adadelta())
+
+feature_cols = training_set[FEATURES]
+labels = training_set[LABEL].values
+
+model.fit(np.array(feature_cols), np.array(labels), epochs=300, batch_size=5)
+
+# Evaluation on the test set created by train_test_split
+model.evaluate(np.array(feature_cols), np.array(labels))
+
+y_predict = model.predict(np.array(test))
+
+sub = pd.DataFrame()
+sub['Id'] = test_ID
+sub['SalePrice'] = prepro_y.inverse_transform(np.array(y_predict).reshape(len(y_predict),1))
+sub.to_csv('submission.csv',index=False)
